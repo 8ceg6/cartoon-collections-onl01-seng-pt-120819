39280eb64630e988151a05711a0fe636c92a4ade
cedric e goddard <8ceg6@comcast.net>
2019-12-16 11:56:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-      array.find do |list|
+      array.select do |list|
             cheese_types.include?(list)
       end
       binding.pry
