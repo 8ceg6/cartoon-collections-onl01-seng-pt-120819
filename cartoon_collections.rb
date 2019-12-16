6ec8aa8668dfc84dcc5cecee04c4801cb5dfdf89
cedric e goddard <8ceg6@comcast.net>
2019-12-16 11:22:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(dwarves)
+    dwarves.each_with_index do |name , index|
+          puts "#{index +1} #{name}"
+          end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(call)
+        summon = []
+        call.collect do |calls|
+          summon <<  calls.capitalize + "!"
+           end
+        summon 
+        
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(extend)
+        extend.map do |long|
+          if long.length > 4 
+        end 
+        binding.pry
 end
 
 def find_the_cheese# code an argument here
