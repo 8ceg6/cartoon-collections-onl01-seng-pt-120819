c5749954b7a56d093cca1b320cfe46d197ea8cd4
cedric e goddard <8ceg6@comcast.net>
2019-12-16 11:35:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,13 @@ def long_planeteer_calls(array)
         # binding.pry
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+      array.include? 
 end
+
+
+
+
+
